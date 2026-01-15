5bc24ac4403b3782e5113476c95ba2daa0b4beab
beamendivil <bea@curatedtucson.com>
2026-01-15 16:01:02 -0700

    Remove frontend files and convert to backend-only Express project
    - Remove all React components, assets, and frontend source code
    - Delete duplicate .eslintrc.js file, keep .eslintrc.cjs
    - Update package.json: remove React dependencies, Vite, and frontend scripts
    - Update ESLint config to remove React-specific rules and settings
    - Clean up project to focus on Express.js backend with MongoDB

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,39 +5,28 @@ module.exports = {
     node: true,
     browser: true,
   },
-  ignorePatterns: ["dist", ".eslintrc.js"],
+  ignorePatterns: ["dist"],
   // We extend airbnb and prettier to ensure they work together
   extends: [
     "eslint:recommended",
     "airbnb-base",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
     "prettier",
   ],
-  plugins: ["react", "react-hooks"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   settings: {
     "import/resolver": {
       node: {
-        extensions: [".js", ".jsx"],
+        extensions: [".js"],
       },
-    },
-    react: {
-      version: "18.2",
     },
   },
   rules: {
     // Mini-task solution: Allow the use of _id (underscore dangle)
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "react/prop-types": "off",
     "no-console": "off",
     "no-new": "off",
     "import/no-extraneous-dependencies": [
@@ -45,7 +34,6 @@ module.exports = {
       {
         devDependencies: [
           "server.js",
-          "vite.config.js",
           "**/*.test.js",
           "**/*.spec.js",
         ],
@@ -56,7 +44,6 @@ module.exports = {
       "ignorePackages",
       {
         js: "always",
-        jsx: "always",
       },
     ],
   },
@@ -68,20 +55,6 @@ module.exports = {
       files: [".eslintrc.{js,cjs}"],
       parserOptions: {
         sourceType: "script",
-      },
-    },
-    // Frontend (Vite/React) uses extension-less imports; allow that in src/
-    {
-      files: ["src/**/*.{js,jsx}", "src/**"],
-      rules: {
-        "import/extensions": [
-          "error",
-          "ignorePackages",
-          {
-            js: "never",
-            jsx: "never",
-          },
-        ],
       },
     },
   ],
--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-const apiKey = "28cc2fcbca4cabff724cc7482422fd1a";
-const latitude = 40.7128;
-const longitude = -74.006;
-
-export { apiKey, latitude, longitude };
